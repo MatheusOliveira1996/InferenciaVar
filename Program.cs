0d48227d13e4397bc0e0b16035fd8e3188ccd8f4
MatheusOliveira1996 <matheus.hortolandia@gmail.com>
2022-09-25 22:03:03 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InferenciaVar {
+    internal class Program {
+        static void Main (string[] args) {
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
